623f67a8a9807d7a2d39c4d76a3700f7ca1660c9
Palash Rambhia <palashrambhia2@gmail.com>
2022-02-27 17:07:32 +0530

    Added Components and Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home'
+import Login from './Components/Login'
+import Signup from './Components/Signup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home/>}></Route>
+            {/* <Route exact path="/AllCat" element={<AllCat/>}></Route> */}
+            <Route exact path="/login" element={<Login/>}></Route>
+            <Route exact path="/signup" element={<Signup/>}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
