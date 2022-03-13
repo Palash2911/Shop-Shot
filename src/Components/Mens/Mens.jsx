25fbe4f0120183bd5c1750f41b57be9b9702bd30
Palash Rambhia <palashrambhia2@gmail.com>
2022-03-13 15:52:37 +0530

    Buttons Removed

--- src/Components/Mens/Mens.jsx
+++ src/Components/Mens/Mens.jsx
@@ -10,7 +10,7 @@ const Mens = () => {
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+           
         </div>
       </div>
        <div className="card men_card">
@@ -18,7 +18,7 @@ const Mens = () => {
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+           
         </div>
       </div>
        <div className="card men_card">
@@ -26,7 +26,7 @@ const Mens = () => {
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+           
         </div>
       </div>
     </div>
