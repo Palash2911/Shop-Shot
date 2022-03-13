d3bba6a87e93c7c6d1eebc3ca94da2bebd9e061a
Palash Rambhia <palashrambhia2@gmail.com>
2022-03-13 15:51:28 +0530

    Multiple Card Added

--- src/Components/Women/Women.jsx
+++ src/Components/Women/Women.jsx
@@ -4,7 +4,23 @@ import '../Women/Women.css'
 
 const Women = () => {
   return (
-    <div>
+    <div className="women_body">
+       <div className="card women_card">
+        <img src={women} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+       <div className="card women_card">
+        <img src={women} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
        <div className="card women_card">
         <img src={women} className="card-img-top" alt="..." />
         <div className="card-body">
