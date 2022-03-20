ba9129f79f317ba095cab7d77495666e3ceebbdb
Palash Rambhia <palashrambhia2@gmail.com>
2022-03-20 17:17:59 +0530

    Hover Effect Added

--- src/Components/Homefiles/Home.jsx
+++ src/Components/Homefiles/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import img from '../Homefiles/img.png'
 import '../Homefiles/Home.css'
 import Books from '../Books/Books'
 import Electronics from '../Electronic/Electronics'
@@ -13,18 +12,20 @@ const Home = () => {
           <h4>Your Shopping Arena</h4>
           <h1>Shop Shot</h1>
       </div>
-      <div className="book_container my-5">
-          <Books/>
-      </div>
-      <div className="men_container my-5">
-          <Mens/>
-      </div>
-      <div className="women_container my-5">
-          <Women/>  
-      </div>
-      <div className="elec_container my-5">
-          <Electronics/>  
-      </div>     
+      <div className="all_cato">
+        <div className="book_container my-5">
+            <Books/>
+        </div>
+        <div className="men_container my-5">
+            <Mens/>
+        </div>
+        <div className="women_container my-5">
+            <Women/>  
+        </div>
+        <div className="elec_container my-5">
+            <Electronics/>  
+        </div> 
+      </div>    
     </>
   )
 }
